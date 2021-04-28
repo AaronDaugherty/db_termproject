478de68d5132c7b950170346c0d84d0852ca4996
Aaron Daugherty <aaronchancedaugherty@gmail.com>
2021-04-27 23:34:30 -0400

    initial commit

---
+++ Spotifate/src/main/java/STD/termproject/Spotifate/SpotifateApplication.java
@@ -0,0 +1,13 @@
+package STD.termproject.Spotifate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotifateApplication.class, args);
+	}
+
+}
---
+++ Spotifate/src/test/java/STD/termproject/Spotifate/SpotifateApplicationTests.java
@@ -0,0 +1,13 @@
+package STD.termproject.Spotifate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
