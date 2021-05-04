e57a7ccb2d0bf1f4789217eb2f379acd519ae052
wcorder1 <gcorder99@gmail.com>
2021-05-04 17:35:23 -0400

    Began working on stable queries with search bar

--- Spotifate/src/main/java/STD/termproject/Spotifate/controller/MyController.java
+++ Spotifate/src/main/java/STD/termproject/Spotifate/controller/MyController.java
@@ -3,12 +3,14 @@ package STD.termproject.Spotifate.controller;
 import STD.termproject.Spotifate.model.Data_by_artist;
 import STD.termproject.Spotifate.service.IData_by_artistService;
 import STD.termproject.Spotifate.model.Data_by_genres;
+import STD.termproject.Spotifate.model.Query;
 import STD.termproject.Spotifate.service.IData_by_genresService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MyController {
@@ -29,7 +31,8 @@ public class MyController {
 
     @Autowired
     private IData_by_genresService Data_by_genresService;
-      @GetMapping("/genres")
+
+    @GetMapping("/genres")
     public String findGenres(Model model) {
 
         var data = (List<Data_by_genres>) Data_by_genresService.findAll();
@@ -37,6 +40,23 @@ public class MyController {
         model.addAttribute("data", data);
 
         return "showGenres";
+    }
 
+    @PostMapping("/query")
+    public String search(Query query, Model model) {
+
+        //@Query(value="SELECT devname,hrs,ot FROM imaginaryTable",nativeQuery=true)
+        //private List<Object> getValues();
+
+        String toQuery = "SELECT " + query.getSelect() + " FROM " + query.getFrom() + " WHERE " + query.getWhere() + " " + query.getOperator() + " " + query.getSearch();
+        System.out.println(toQuery);
+/*
+        @Query(value="SELECT %s FROM %s WHERE %s %s %s", 
+            (query.getSelect(), query.getFrom(), query.getWhere(),
+            query.getOperator(), query.getSearch()), nativeQuery=true)
+
+        private List<Object> getValues();
+*/
+        return "showQuery";
     }
 }
